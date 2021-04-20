4d660b02ba5efafa986c8c7a3a7e5cc3804a13d5
Paperninja20 <51069791+Paperninja20@users.noreply.github.com>
2021-04-19 20:22:19 -0700

    added comment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-// game configuration
+//Jacob Yu, Mellow Fishing, 4/19/2021, about 15 hours
+//Points breakdown:
+//  10 points for disaplying the time in seconds on the screen
+//  20 points for creating a new spaceship type that's smaller, moves faster, and is worth more points
+//  20 points for implementing a new timing/scoring mechanism that adds time to the clock for successful hits
+//  60 points for redesigning the game's artwork, UI, and sound to an underwater fishing theme.
 let config = {
     type: Phaser.CANVAS,
     width: 640,
